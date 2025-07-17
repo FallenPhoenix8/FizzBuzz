aa99b0b152368a0a440630399372a914f1ed86e5
FallenPhoenix8 <tplayxyz8@gmail.com>
2025-07-17 18:20:27 +0200

    Initial Commit

---
+++ FizzBuzz/FizzBuzzApp.swift
@@ -0,0 +1,16 @@
+// 
+//  FizzBuzzApp.swift
+//  FizzBuzz
+//
+    
+
+import SwiftUI
+
+@main
+struct FizzBuzzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
